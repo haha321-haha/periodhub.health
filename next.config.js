80e59485d3ffddfebcfc09c0d6a003df48a9fd7a
nodownloadgames.github.io <dutinglove@gmail.com>
2025-04-15 23:52:08 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
+/​**​ @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
     domains: ['images.unsplash.com']
+  },
+  async redirects() {
+    // 仅在生产环境启用重定向
+    if (process.env.VERCEL_ENV === 'production') {
+      return [
+        {
+          source: '/:path*',
+          destination: 'https://www.periodhub.health/:path*',
+          permanent: true // 301 永久重定向
+        }
+      ]
+    }
+    return []
   }
-};
+}
 
 module.exports = nextConfig;

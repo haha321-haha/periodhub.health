d757af45e9df0a33e862bfb6c2de9aa8c99f73ec
nodownloadgames.github.io <dutinglove@gmail.com>
2025-04-23 14:41:29 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/magic-system/',
   plugins: [react()],
-  server: {
-    port: 5173, // 设置开发服务器端口
-  },
   build: {
-    outDir: 'dist', // 输出目录
+    outDir: 'dist',
+    assetsInlineLimit: 4096,
     rollupOptions: {
-      input: 'src/main.tsx', // 明确指定入口文件
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
     },
+    minify: 'terser',
+    sourcemap: true
   },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  }
 });
